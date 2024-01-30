7d46d3419e694abbac3857f7cd9fc4ba20bff6eb
frizz <yan.davidovich@gmail.com>
2024-01-30 08:19:45 +0000

    fixing wireguard

--- ct/wireguard.sh
+++ ct/wireguard.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-source <(curl -s https://raw.githubusercontent.com/frizzby/Proxmox-wireguard-fix/main/gmisc/build.func)
+source <(curl -s https://raw.githubusercontent.com/frizzby/Proxmox-wireguard-fix/main/misc/build.func)
 # Copyright (c) 2021-2024 tteck
 # Author: tteck (tteckster)
 # License: MIT
